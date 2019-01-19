865126a896653c634af492b2c393fd62ea6d94d1
Christian Berck <christian@berck.org>
2019-01-19 15:40:02 -0600

    Implemented spinner and other stuff

--- Farm123/app/src/main/java/com/example/pappy_000/farm123/FieldMenu.java
+++ Farm123/app/src/main/java/com/example/pappy_000/farm123/FieldMenu.java
@@ -1,13 +1,22 @@
 package com.example.pappy_000.farm123;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FieldMenu extends AppCompatActivity {
+    List<String> spinnerArray =  new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,23 @@ public class FieldMenu extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });
+        spinnerArray.add("item1");
+        spinnerArray.add("item2");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, spinnerArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner sItems = (Spinner) findViewById(R.id.spinner2);
+        sItems.setAdapter(adapter);
+
+        Button next = (Button) findViewById(R.id.button4);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FieldMenu.this, SingleFieldScreen.class));
+            }
+
         });
     }
 
--- Farm123/app/src/main/java/com/example/pappy_000/farm123/MainMenu.java
+++ Farm123/app/src/main/java/com/example/pappy_000/farm123/MainMenu.java
@@ -1,11 +1,14 @@
 package com.example.pappy_000.farm123;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -24,6 +27,15 @@ public class MainMenu extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        ImageButton next = (ImageButton) findViewById(R.id.imageButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenu.this, FieldMenu.class));
+            }
+
+        });
     }
+
 
 }
