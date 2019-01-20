e8d4e2f34d88bff0a3ecabc5af9e3b906ec67b58
Chris Burbach <officialchrisb@icloud.com>
2019-01-20 01:31:34 -0600

    Removed vestigial FAB

--- Farm123/app/src/main/java/com/example/pappy_000/farm123/FieldDetails.java
+++ Farm123/app/src/main/java/com/example/pappy_000/farm123/FieldDetails.java
@@ -13,17 +13,8 @@ public class FieldDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
