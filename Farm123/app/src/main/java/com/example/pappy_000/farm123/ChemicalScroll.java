86bcbffa0529bf6c775b7297ce59dc6b97fcd104
Christian Berck <christian@berck.org>
2019-01-19 22:05:53 -0600

    Adding Chemical Scroll

--- Farm123/app/src/main/java/com/example/pappy_000/farm123/ChemicalScroll.java
+++ Farm123/app/src/main/java/com/example/pappy_000/farm123/ChemicalScroll.java
@@ -1,20 +1,18 @@
 package com.example.pappy_000.farm123;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class SingleFieldScreen extends AppCompatActivity {
+public class ChemicalScroll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_field_screen);
+        setContentView(R.layout.activity_chemical_scroll);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,14 +24,5 @@ public class SingleFieldScreen extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        Button chems = findViewById(R.id.button2);
-        chems.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SingleFieldScreen.this, ChemicalScroll.class));
-            }
-
-        });
     }
-
 }
